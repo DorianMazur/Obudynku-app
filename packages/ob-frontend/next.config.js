3bbcf664d89c9d4e02aadd736319b1f43859378f
MazurDorian <mazur.dorian15@gmail.com>
2022-11-24 00:03:12 +0100

    fix: Dockerfile and standalone next build

--- packages/ob-frontend/next.config.js
+++ packages/ob-frontend/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
+  experimental: {
+    // this includes files from the monorepo base two directories up
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
 };
 
 module.exports = nextConfig;
