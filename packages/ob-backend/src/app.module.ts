07871120f58ef226212c761c4d77b5186bd553fd
MazurDorian <mazur.dorian15@gmail.com>
2023-09-14 10:30:19 +0100

    fix: ob-backend docker image for arm64

--- packages/ob-backend/src/app.module.ts
+++ packages/ob-backend/src/app.module.ts
@@ -5,15 +5,29 @@ import { AuthModule } from './auth/auth.module';
 import { HealthModule } from './health/health.module';
 import { OpinionModule } from './opinion/opinion.module';
 import { BuildingModule } from './building/building.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'better-sqlite3',
-      database: ':memory:',
-      dropSchema: true,
-      entities: ['dist/**/*.entity{.ts,.js}'],
-      synchronize: true,
+    process.env.NODE_ENV === 'production'
+      ? TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: process.env.MYSQL_HOST,
+          port: process.env.MYSQL_PORT,
+          username: process.env.MYSQL_USERNAME,
+          password: process.env.MYSQL_PASSWORD,
+          database: process.env.MYSQL_DATABASE,
+          entities: ['dist/**/*.entity{.ts,.js}'],
+        })
+      : TypeOrmModule.forRoot({
+          type: 'better-sqlite3',
+          database: ':memory:',
+          dropSchema: true,
+          entities: ['dist/**/*.entity{.ts,.js}'],
+          synchronize: true,
+        }),
+    ConfigModule.forRoot({
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
     }),
     UserModule,
     AuthModule,
